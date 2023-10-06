9737a0b961d3a376f75d3e8d952ef78b8b38805f
anshika sharma <anshikasharma.as19@gmail.com>
2023-10-06 17:34:26 +0530

    Developed a website named WeCare

---
+++ script.js
@@ -0,0 +1,9 @@
+function openlogin() {
+    document.querySelector("#mylogin").style.display="block";
+    document.querySelector("#blur").style.display="block";
+}
+
+function closelogin() {
+    document.querySelector("#mylogin").style.display="none";
+    document.querySelector("#blur").style.display="none";
+}
